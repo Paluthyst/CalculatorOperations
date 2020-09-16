329602f35bb77ca7ca0076657a81280f5f4ccf9c
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-09-16 19:21:19 -0400

    Add project files.

---
+++ CalculatorOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace CalculatorOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //indetify num1
+            int num1;
+            //user input
+            Console.WriteLine("Can I have a number?");
+            num1 = int.Parse(Console.ReadLine());
+            //operations
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+            Console.WriteLine(num1 + " plus 1 is: " + (num1 + 1));
+            Console.WriteLine(num1 + " subtracted by 1 is: " + (num1 - 1));
+            Console.WriteLine(num1 + " divided by 1 is: "+ (num1 % 1));
+            Console.WriteLine(num1 + " multiplyed by 1 is: " + (num1*1));
+            Console.WriteLine(num1 + " ++ is: "+ (num1 + 1));
+            Console.WriteLine(num1 + " -- is: "+ (num1 - 1));
+
+        }
+    }
+}
